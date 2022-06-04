d103d1d9872a1ab148a3d589c8a2eaa17afc464b
CA Meijer <hammingweight@gmail.com>
2022-06-04 18:31:56 +0200

    Allow the build script to be run from anywhere.

--- build/build.sh
+++ build/build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -e
+script_dir="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )"
+
 minikube delete
 minikube start
 
 eval $(minikube docker-env)
-docker build -t httpserver:1.0 ../containers/server
-docker build -t fim:1.0 ../containers/fim
+docker build -t httpserver:1.0 $script_dir/../containers/server
+docker build -t fim:1.0 $script_dir/../containers/fim
 
-kubectl apply -f ../k8s_resources/serverpod.yaml
-kubectl apply -f ../k8s_resources/service.yaml
+kubectl apply -f $script_dir/../k8s_resources/serverpod.yaml
+kubectl apply -f $script_dir/../k8s_resources/service.yaml
