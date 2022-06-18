503b99da6f9450039491e97efbd0cc8255b625f1
CA Meijer <hammingweight@gmail.com>
2022-06-18 08:36:11 +0200

    Use canonical image names.

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ script_dir="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && 
 minikube start
 
 eval $(minikube docker-env)
-docker build -t hello_server:1.0 $script_dir/containers/hello_server
-docker build -t fim:1.0 $script_dir/containers/fim
+docker build -t hammingweight/hello_server:1.0.0 $script_dir/containers/hello_server
+docker build -t hammingweight/fim:1.0.0 $script_dir/containers/fim
 
 kubectl apply -f $script_dir/k8s_manifests/hello_server_pod.yaml
 kubectl apply -f $script_dir/k8s_manifests/hello_load_balancer_service.yaml
