55ddefa192d570f8b45a0395b209583eb27ea815
CA Meijer <hammingweight@gmail.com>
2024-02-04 15:57:30 +0200

    Add security context to the FIM sidecar.

--- terraform/pod.tf
+++ terraform/pod.tf
@@ -5,12 +5,23 @@ resource "kubernetes_pod" "hello-pod" {
   }
 
   spec {
+    share_process_namespace = true
     container {
       image = "docker.io/hammingweight/hello_server:1.0.0"
       name  = "hello-server"
 
       port {
         container_port = 8000
+      }
+    }
+    container {
+      image = "docker.io/hammingweight/fim:1.0.0"
+      name  = "fim"
+      security_context {
+        capabilities {
+          drop = ["ALL"]
+          add  = ["DAC_READ_SEARCH", "KILL", "SYS_PTRACE"]
+        }
       }
     }
   }
