cefe5ce18899dfe7b0fe148ef81175283c596fd0
Carl Meijer <hammingweight@gmail.com>
2024-04-05 14:14:49 +0200

    Push images to minikube in the image way.

--- build.sh
+++ build.sh
@@ -4,9 +4,13 @@ script_dir="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && 
 
 minikube start
 
-eval $(minikube -p minikube docker-env)
-docker build -t hammingweight/hello_server:1.0.0 $script_dir/containers/hello_server
-docker build -t hammingweight/fim:1.0.0 $script_dir/containers/fim
+HELLO_IMAGE_NAME=hammingweight/hello_server:1.0.0
+docker build -t $HELLO_IMAGE_NAME $script_dir/containers/hello_server
+minikube image load $HELLO_IMAGE_NAME
+
+FIM_IMAGE_NAME=hammingweight/fim:1.0.0
+docker build -t $FIM_IMAGE_NAME $script_dir/containers/fim
+minikube image load $FIM_IMAGE_NAME
 
 kubectl apply -f $script_dir/k8s_manifests/hello_server_pod.yaml
 kubectl apply -f $script_dir/k8s_manifests/hello_load_balancer_service.yaml
