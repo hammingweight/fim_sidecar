1030d998c9109ccac3f1fa8806f3dac195463da7
Carl Meijer <hammingweight@gmail.com>
2024-04-06 18:59:22 +0200

    Use a deployment.

--- build.sh
+++ build.sh
@@ -12,5 +12,5 @@ FIM_IMAGE_NAME=hammingweight/fim:1.0.0
 docker build -t $FIM_IMAGE_NAME $script_dir/containers/fim
 minikube image load $FIM_IMAGE_NAME
 
-kubectl apply -f $script_dir/k8s_manifests/hello_server_pod.yaml
+kubectl apply -f $script_dir/k8s_manifests/hello_server_deployment.yaml
 kubectl apply -f $script_dir/k8s_manifests/hello_load_balancer_service.yaml
