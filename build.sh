36b48200ac09c4bbfb509875cf43373cda49dfc8
Carl Meijer <hammingweight@gmail.com>
2024-04-08 14:09:04 +0200

    Start on support for microk8s.

--- build.sh
+++ build.sh
@@ -2,7 +2,14 @@
 set -e
 script_dir="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )"
 
-minikube start
+if [ -z ${USE_MICROK8S} ]
+then
+    minikube start
+else
+    microk8s start
+    cp $HOME/.kube/config $HOME/.kube/config.bak
+    microk8s config > $HOME/.kube/config
+fi
 
 HELLO_IMAGE_NAME=hammingweight/hello_server:1.0.0
 docker build -t $HELLO_IMAGE_NAME $script_dir/containers/hello_server
@@ -14,5 +21,5 @@ docker build -t $FIM_IMAGE_NAME $script_dir/containers/fim
 minikube image load $FIM_IMAGE_NAME
 docker rmi $FIM_IMAGE_NAME
 
-kubectl apply -f $script_dir/k8s_manifests/hello_server_pod.yaml
-kubectl apply -f $script_dir/k8s_manifests/hello_load_balancer_service.yaml
+kubectl create -f $script_dir/k8s_manifests/hello_server_pod.yaml
+kubectl create -f $script_dir/k8s_manifests/hello_load_balancer_service.yaml
